0cdb35b0b8b549420814c0501dcf9004111e0f94
giiTamara <claro.giovana@escola.pr.gov.br>
2025-10-03 12:07:57 -0300

    update

--- app.js
+++ app.js
@@ -4,13 +4,13 @@ let foto_troca = false;
 function trocaFoto() {
 
     if (foto_troca === false) {
-        for (let i = 0; i > imagem.length; i++) {
+        for (let i = 0; i < imagem.length; i++) {
             imagem[i].src = `lana(${i + 8}).jpeg`;
         }
         foto_troca = true;
     } else {
         for (let i = 0; i > imagem.length; i++) {
-            
+
                 imagem[i].jpeg = `i${i + 1}.jpeg`
             
         }
