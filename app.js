a0324f1cf4594223cc65602edd7e558424ca0216
giiTamara <claro.giovana@escola.pr.gov.br>
2025-10-03 12:03:17 -0300

    update

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const imagem = document.querySelectorAll('img');
 let foto_troca = false;
 
-function trocaFoto(){
-    for(let i = 0; i > imagem.length; i++){
-        if(foto_troca === false){
+function trocaFoto() {
+
+    if (foto_troca === false) {
+        for (let i = 0; i > imagem.length; i++) {
             imagem[i].src = `lana(${i + 8}).jpeg`;
-            foto_troca = true;
-        }else{
-       for(let i = 0; i > imagem.length; i++){
-        if(foto_troca === false){
-            imagem[i].src = `lana(${i + 1}).jpeg`;
-            foto_troca = false;
         }
-                    }
+        foto_troca = true;
+    } else {
+        for (let i = 0; i > imagem.length; i++) {
+            if (foto_troca === false) {
+                imagem[i].jpeg = `i${i + 1}.jpeg`
+                
+            }
+            foto_troca = false;
         }
     }
 }
